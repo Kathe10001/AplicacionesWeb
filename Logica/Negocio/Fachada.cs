4be37bafa1509c19aefa2113e15338d7b8924e31
Fabián Pérez <PERETIAS18@gmail.com>
2022-11-04 20:47:04 -0300

    4/11/2022

--- Logica/Negocio/Fachada.cs
+++ Logica/Negocio/Fachada.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Logica.VOs;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,41 @@ namespace Logica.Negocio
 {
     class Fachada //hacer despues
     {
+
+
+        public List<VOAlbum> ListadoAlbum() { throw new NotImplementedException(); }
+
+        public void AltaAlbum(VOAlbum album) { }
+
+        public void BajaAlbum(int idAlbum) { }
+
+        public void AgregarCancionAlbum(VOCancion cancion, VOAlbum album) { }
+
+        public void QuitarCancionAlbum(int idCancion, VOAlbum album) { }
+
+        public List<VOBanda> ListarBandas(VOBanda banda) { throw new NotImplementedException(); }
+
+        public void AltaBanda(VOBanda banda) { }
+
+        public void BajaBanda(int idBanda) { }
+
+        public void AgregarIntegranteBanda(VOIntegrante integrante, VOBanda banda) { }
+
+        public void QuitarIntegranteBanda(int idIntegrante, VOBanda banda) { }
+
+        public List<VOCancion> ListarCanciones() { throw new NotImplementedException(); }
+
+        public void AltaCancion(VOCancion cancion) { }
+
+        public void BajaCancion(int idCancion) { }
+
+        public List<VOIntegrante> ListarIntegrante() { throw new NotImplementedException(); }
+
+        public void AltaIntegrante(VOIntegrante integrante) { }
+
+        public void BajaIntegrante(int idIntegrante) { }
+
+
 
     }
 }
--- WebServices/WebService1.asmx.cs
+++ WebServices/WebService1.asmx.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Services;
 
+
 namespace WebServices
 {
     /// <summary>
@@ -18,9 +19,51 @@ namespace WebServices
     {
 
         [WebMethod]
-        public string HelloWorld()
-        {
-            return "Hola a todos";
-        }
+        public List<VOAlbum> ListadoAlbum() { throw new NotImplementedException(); }
+
+        [WebMethod]
+        public void AltaAlbum(VOAlbum album) { }
+
+        [WebMethod]
+        public void BajaAlbum(int idAlbum) { }
+
+        [WebMethod]
+        public void AgregarCancionAlbum(VOCancion cancion, VOAlbum album) { }
+
+        [WebMethod]
+        public void QuitarCancionAlbum(int idCancion, VOAlbum album) { }
+
+        [WebMethod]
+        public List<VOBanda> ListarBandas(VOBanda banda) { throw new NotImplementedException(); }
+
+        [WebMethod]
+        public void AltaBanda(VOBanda banda) { }
+
+        [WebMethod]
+        public void BajaBanda(int idBanda) { }
+
+        [WebMethod]
+        public void AgregarIntegranteBanda(VOIntegrante integrante, VOBanda banda) { }
+
+        [WebMethod]
+        public void QuitarIntegranteBanda(int idIntegrante, VOBanda banda) { }
+
+        [WebMethod]
+        public List<VOCancion> ListarCanciones() { throw new NotImplementedException(); }
+
+        [WebMethod]
+        public void AltaCancion(VOCancion cancion) { }
+
+        [WebMethod]
+        public void BajaCancion(int idCancion) { }
+
+        [WebMethod]
+        public List<VOIntegrante> ListarIntegrante() { throw new NotImplementedException(); }
+
+        [WebMethod]
+        public void AltaIntegrante(VOIntegrante integrante) { }
+
+        [WebMethod]
+        public void BajaIntegrante(int idIntegrante) { }
     }
 }
